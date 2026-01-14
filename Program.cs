5ca4544b3c992d761412a4ba1babbaa0fdfcc55c
Aidan Litschi <AidLit766@bctcstudent.org>
2026-01-14 13:45:00 -0500

    Added frmStockInput

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Stock_Checker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
